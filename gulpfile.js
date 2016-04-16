e8c540de16fa421aae6c1d47eb5c77cf175380a3
Maciej Osytek <maciej.osytek@vox.pl>
2016-04-16 14:29:05 +0200

    Gulp browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
-var inject = require('gulp-inject');
- 
-gulp.task('index', function () {
-  var target = gulp.src('./src/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths: 
-  var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {read: false});
- 
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./src'));
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './app'
+    }
+  });
+
+  gulp.watch(['**/*.html', 'css/**/*.css', '**/*.js'], {cwd: 'app'}, reload);
 });
