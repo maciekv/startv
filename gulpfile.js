e79c59f3f9fefa33d89f82d8a71ac47909f4b7fe
Maciej Osytek <maciej.osytek@vox.pl>
2016-04-17 01:03:52 +0200

    Gulp +=plumber + sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,48 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'), // przeladowuje przegladarke
+    autoprefixer = require('gulp-autoprefixer'), // dodaje prefiksy css like -webkit-*
+    plumber = require('gulp-plumber'), // powoduje, ze jesli wystapia bledy, nasluchiwanie nie jest przerywane
+    sass = require('gulp-sass') // kompiluje sass to scss
+;
+
 var reload = browserSync.reload;
+gulp.task('hello', function() {
+    console.log('hello!');
+})
+
+gulp.task('css', function() {
+
+    gulp
+        .src("app/sass/main.scss")
+        .pipe(plumber())
+        // .pipe(sass()) // trzeba uzyc sass.sync(), jesli korzystamy z plumbera 
+        .pipe(sass.sync())
+        .pipe(autoprefixer({
+            browsers: ["last 5 version", "IE 9"]
+        }))
+        .pipe(gulp.dest("app/css/"))
+        .pipe(browserSync.stream());
+
+})
 
 // watch files for changes and reload
 gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: './app'
-    }
-  });
 
-  gulp.watch(['**/*.html', 'css/**/*.css', '**/*.js'], {cwd: 'app'}, reload);
+    browserSync({
+        server: {
+            baseDir: './app'
+        }
+    });
+
+    // nasluchuj zmian w scss i wykonuj zadanie css
+    gulp.watch("app/sass/**/*.scss", ["css"]);
+
+    // nasluchuj zmian i reload przegladarki
+    gulp.watch(['**/*.html', 'css/**/*.css', '**/*.js'], {
+        cwd: 'app'
+    }, reload);
 });
+
+
+
+gulp.task("default", ["css", "serve"]);
